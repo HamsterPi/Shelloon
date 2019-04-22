4349c8d9a333a8d22192481378ca1555a76a1622
Connell Kelly <32110129+HamsterPi@users.noreply.github.com>
2019-04-22 23:01:51 +0100

    Version 0.7

--- myshell.py
+++ myshell.py
@@ -1,7 +1,5 @@
-#Shelloon - Connell Kelly
+#Shelloon by Connell Kelly
 #Language: Python 3.7
-#I hope the cowboy theme don't sour your milk.
-#My thanks for a good semester.
 
 #Relevant modules.
 from cmd import Cmd
@@ -11,10 +9,46 @@ import pathlib
 import platform
 import readline
 import socket
+import subprocess
 import sys
+
+def overwrite(data, args):
+	"""Overwrites file with info or creates file containing input.\n"""
+	with open(args[0], "w+") as args:
+		for i in data:
+			#Writes data to file.
+			args.write(i)
+			#Adds newline character to end of file.
+			args.write("\n")
+
+def append(data, args):
+	"""Appends input to file or creates file containing input.\n"""
+	with open(args[0], "a+") as args:
+		for i in data:
+			args.write(i)
+			args.write("\n")
 
 #Shell class and functions.
 class Shelloon(Cmd):
+	def default(self, args):
+		"""Changes to subprocess if using undefined function.\n"""
+		proclist = args.split()
+		if proclist[-1] == "&":
+			#Prepares each as a background process.
+			for i in range(0, len(proclist[:-1])):
+				if proclist[i] == ">":
+					#Overwrite output.
+					overwrite(subprocess.Popen(proclist[:i]), proclist[i+1:])
+				elif proclist[i] == ">>":
+					#Append output.
+					append(subprocess.Popen(proclist[:i], proclist[i+1]))
+				else:
+					#Otherwise, look to ampersand.
+					subprocess.Popen(proclist[:-1])
+		else:
+			#If ampersand is not present.
+			subprocess.run(proclist)
+
 	def do_cd(self, args):
 		"""Changes current default directory to directory requested. If it doesn't exist, report current directory.\n"""
 		try:
@@ -67,35 +101,41 @@ class Shelloon(Cmd):
 
 	def do_quit(self, args):
 		"""Quits the program."""
-		print("So long, regular cowboy.")
+		print("Happy trails!\nQuitting prompt...")
 		raise SystemExit
 
 def batch_file():
 	"""Identifies and reads commands from a text file."""
 	try:
+		#Read batch file as args.
 		with open(sys.argv[1], 'r') as args:
 			shell = Shelloon()
-			commandlines = args.readlines() 
+			#Collect commands.
+			commandlines = args.readlines()
+			#Allows prompt to end after txt commands.
 			commandlines.append('quit')
+			#Queue all batch file commands.
 			shell.cmdqueue = commandlines
+			#Loop through each command and end.
 			shell.cmdloop()
 	except IndexError:
+			#If there's no file, move on to prompt.
 			pass
 
 if __name__ == "__main__":
+	#Call function and check if batch file is present.
 	batch_file()
-
+	#Determine whether or not prompt is on Windows or Linux.
 	if platform.system() == "Windows":
 		prompt = Shelloon()
 		prompt.prompt = os.getcwd() + '>'
 		prompt.cmdloop("Starting prompt...\nHowdy, welcome to the Shelloon!\n(Type 'help' for a list of commands.)")
-
 	elif platform.system() == "Linux":
 		user = getpass.getuser()
-		host = socket.gethostname() #need to get colours
-
+		host = socket.gethostname()
 		prompt = Shelloon()
 		prompt.prompt = user + "@" + host + "\nshell=" + os.getcwd() + ":~$ "
 		prompt.cmdloop("Starting prompt...\nHowdy, welcome to the Shelloon!\n(Type 'help' for a list of commands.)\n")
+	#Otherwise, it cannot be supported.
 	else:
 		pass
